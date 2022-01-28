54d9525c34a69500e7374029ac8df36d115173b8
Gaby Manes <gabymanes95@gmail.com>
2022-01-27 22:01:29 -0300

    ultimo

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const path = require('path');
-//const usersFilePath = path.join(__dirname, '../data/usuarios.json');
-//const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
+const usersFilePath = path.join(__dirname, '../data/usuarios.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
 
 const controller = {
     form : (req,res) =>{ 
     res.render ("formulario")
     },
 
-    /*crear: (usuario) => {
+    /*
+    HAY ALGO QUE NO ESTA FUNCIONANDO
+    crear: deberia de trabajar con el formulario 
+    {
+     usuario = {
+         id : req.params.id
+         nombre: req.body.nombre
+     }
     users.push(usuario);
     fs.writeFileSync(users, JSON.stringify(users,null,""))
     }
@@ -17,7 +24,10 @@ const controller = {
     users.filter(usuario => usuario.id !== id)
     fs.writeFileSync(users, JSON.stringify(users,null,""))  
     }
+    
+
+    edit: trabajas con el id con lo nuevo que te llega del formulario! 
+    
     */ 
 }
-//console.log(controller.crear({fullname:"jabi",email:"fega@gmail.com"}))
 module.exports = controller
